9ccff8d5b1e394fc376f7b3f1446c0ef7038ce7c
anddeIl <drogadvc@gmail.com>
2023-07-26 16:59:05 +0200

    Create curl.php

---
+++ curl.php
@@ -0,0 +1,28 @@
+<?php
+
+$curl = curl_init();
+
+curl_setopt_array($curl, [
+  CURLOPT_URL => "https://www.machinools/api/annotation/scrape",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "POST",
+  CURLOPT_POSTFIELDS => "{\"text\": \"Cette recette inratable de tomates cerises maison est un must-have dans votre carnet de recettes. \"}",
+  CURLOPT_HTTPHEADER => [
+    "Content-Type: application/json"
+  ],
+]);
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  echo $response;
+}
